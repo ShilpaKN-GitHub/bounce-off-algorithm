7b4f26713132ac1a7f238d5ec2f4b92bfaa58570
ShilpaKN-GitHub <69884599+ShilpaKN-GitHub@users.noreply.github.com>
2020-08-23 22:31:05 +0530

    Update sketch.js
    Made bounceOff() generic function.

--- sketch.js
+++ sketch.js
@@ -1,39 +1,47 @@
 var fixedRect, movingRect;
 
-function setup() {
+function setup()
+{
   createCanvas(1200,800);
 
   fixedRect = createSprite(400,100, 50, 80);
   fixedRect.shapeColor = "green";
   fixedRect.velocityY = +5;
 
-  movingRect = createSprite(400,800,80,30);
+  movingRect = createSprite(400,500,80,30);
   movingRect.shapeColor = "green";
   movingRect.velocityY = -5;
 }
 
-function draw() {
-  background(0,0,0);
+function draw()
+{
+  background(0);
 
-  var xCenterDistance = fixedRect.width/2 + movingRect.width/2;
-  var yCenterDistance = fixedRect.height/2 + movingRect.height/2;
+  bounceOff(movingRect, fixedRect);
 
-  var rightDistance = movingRect.x - fixedRect.x;
-  var leftDistance = fixedRect.x - movingRect.x;
+  drawSprites();
+}
 
-  var topDistance = movingRect.y - fixedRect.y;
-  var bottomDistance = fixedRect.y - movingRect.y;
+function bounceOff(object1, object2)
+{
+  var xCenterDistance = object2.width/2 + object1.width/2;
+  var yCenterDistance = object2.height/2 + object1.height/2;
 
-  if (rightDistance < xCenterDistance && leftDistance < xCenterDistance) {
-    movingRect.velocityX = movingRect.velocityX * (-1);
-    fixedRect.velocityX = fixedRect.velocityX * (-1);
+  var rightDistance = object1.x - object2.x;
+  var leftDistance = object2.x - object1.x;
+
+  var topDistance = object1.y - object2.y;
+  var bottomDistance = object2.y - object1.y;
+
+  if (rightDistance < xCenterDistance && leftDistance < xCenterDistance)
+  {
+    object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1);
   }
 
   if (topDistance < yCenterDistance && bottomDistance < yCenterDistance)
   {
-    movingRect.velocityY = movingRect.velocityY * (-1);
-    fixedRect.velocityY = fixedRect.velocityY * (-1);
+    object1.velocityY = object1.velocityY * (-1);
+    object2.velocityY = object2.velocityY * (-1);
   }
-
-  drawSprites();
 }
